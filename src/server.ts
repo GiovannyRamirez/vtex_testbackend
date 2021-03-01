21aa2ef3c5cf378fd0831049e71277f42522663d
Giovanny <giovannyramirezs@hotmail.com>
2021-03-01 15:35:39 -0500

    Connect city controller with server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import { connect } from './db'
 import vendorRouter from './routes/vendor'
+import cityRouter from './routes/city'
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use('/vendors', vendorRouter)
+app.use('/cities', cityRouter)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
