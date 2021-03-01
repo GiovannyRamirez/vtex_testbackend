117e82bcf3d5b0bba2f4576193906ef36167317b
Giovanny <giovannyramirezs@hotmail.com>
2021-02-28 23:07:47 -0500

    Connect database with server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import { connect } from './db'
 
 dotenv.config()
 
 const port = process.env.PORT || 9000
 const app = express()
+connect()
 
 app.use(cors())
 app.use(express.json())
